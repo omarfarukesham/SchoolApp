a59fac5bb2883ce17847b46c7f1ddab192eaed0d
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-09-20 16:26:53 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
+import Contact from "./components/Contact/Contact";
 import Sliders from "./components/Slider/Sliders";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init()
+  }, [])
   return (
     <div className="App">
       <h2>This is a School App</h2>
       <p>look mama i can code now</p>
-      <Sliders />
+      {/* <Sliders /> */}
+      <Contact />
     </div>
   );
 }
