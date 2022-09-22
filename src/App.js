c26363bdcafdcfc7fdd248b908413183e4f6a97f
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-09-22 15:14:53 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Contact from "./components/Contact/Contact";
 import Sliders from "./components/Slider/Sliders";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Wave from 'react-wavify';
+import Footer from "./components/Shared/Footer";
 
 function App() {
   useEffect(() => {
@@ -11,10 +13,23 @@ function App() {
   }, [])
   return (
     <div className="App">
+
       <h2>This is a School App</h2>
       <p>look mama i can code now</p>
-      {/* <Sliders /> */}
+      <Sliders />
       <Contact />
+      <Wave fill='#0f172a'
+        paused={false}
+        options={{
+          height: 80,
+          amplitude: 30,
+          speed: 0.20,
+          points: 4
+        }}
+      />
+      <Footer>
+
+      </Footer>
     </div>
   );
 }
