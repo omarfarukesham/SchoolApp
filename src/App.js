e6af4777c55adc54b81c6f9a63b6ed8dbefe98cf
Sakib Rahman <seu.sakib@gmail.com>
2022-09-23 20:42:46 +0600

    fix footer

--- src/App.js
+++ src/App.js
@@ -2,34 +2,32 @@ import { useEffect } from "react";
 import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Sliders from "./components/Slider/Sliders";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Wave from 'react-wavify';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Wave from "react-wavify";
 import Footer from "./components/Shared/Footer";
 
 function App() {
   useEffect(() => {
-    AOS.init()
-  }, [])
+    AOS.init();
+  }, []);
   return (
     <div className="App">
-
       <h2>This is a School App</h2>
       <p>look mama i can code now</p>
       <Sliders />
       <Contact />
-      <Wave fill='#0f172a'
+      <Wave
+        fill="#0f172a"
         paused={false}
         options={{
           height: 80,
           amplitude: 30,
-          speed: 0.20,
-          points: 4
+          speed: 0.2,
+          points: 4,
         }}
       />
-      <Footer>
-
-      </Footer>
+      <Footer />
     </div>
   );
 }
