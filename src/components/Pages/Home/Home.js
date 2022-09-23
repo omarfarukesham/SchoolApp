d5ade8dfbb879418a754cfc1f1b25e61b6b1ba23
Sakib Rahman <seu.sakib@gmail.com>
2022-09-24 01:46:58 +0600

    fixed img height & width and optimized sliders img

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Sliders from './Slider/Sliders'
-import Contact from './Contact/Contact'
-import Courses from './Courses/Courses'
+import React from "react";
+import Sliders from "./Slider/Sliders";
+import Contact from "./Contact/Contact";
+import Courses from "./Courses/Courses";
 
 export const Home = () => {
-    return (
-        <div>
-            <Courses/>
-            <Sliders />
-            <Contact />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Courses />
+      <Sliders />
+      <Contact />
+    </div>
+  );
+};
