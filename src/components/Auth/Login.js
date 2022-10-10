7894f918ad62ae671f33fb9807f2bf94bf2a47b0
Sakib Rahman <seu.sakib@gmail.com>
2022-10-10 12:50:23 +0600

    added Loding class

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-
+import auth from "../../firebase.init";
+// import { useAuthState } from "react-firebase-hooks/auth";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import Loading from "../Shared/Loading";
 const Login = () => {
   //REGEX pattern
   const EMAIL_PATTERN = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-z]+)$/;
   // Min length 8 and at least 1 latter
   const PASSWORD_PATTERN = /^(?=.*\d).{8,}$/;
 
+  const [signInWithGoogle, user, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  googleError && console.log("error", googleError.code);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
+
   const onSubmit = () => {
-    console.log("login");
+    googleError && console.log("login");
   };
   return (
     <div className="bg-[url('/src/asset/AuthImg/loginImg1.jpg')] bg-cover bg-no-repeat bg-center">
@@ -30,23 +37,32 @@ const Login = () => {
               <div className="flex gap-4 item-center">
                 {/* Google button */}
                 <button
+                  onClick={() => signInWithGoogle()}
                   type="button"
                   className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="mr-2"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
-                  </svg>
+                  {googleLoading ? (
+                    <Loading size={18} className={"mr-2"} />
+                  ) : (
+                    <svg
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="mr-2"
+                      viewBox="0 0 1792 1792"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
+                    </svg>
+                  )}
                   Google
                 </button>
               </div>
-
+              {googleError && (
+                <span className="text-sm text-red-500 ml-10 mt-2">
+                  {googleError.code}
+                </span>
+              )}
               <div className="flex mt-7 items-center text-center">
                 <hr className="border-gray-300 border-1 w-full rounded-md" />
                 <label className="block font-medium text-sm text-gray-600 w-full">
