ba639e94a55100fabda9300c1c7af9fa723a3101
Sakib Rahman <seu.sakib@gmail.com>
2022-11-05 23:21:29 +0600

    implemented dashboard sidebar

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* <!-- Page content here --> */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-black bg-opacity-90 text-white">
+            {/* <!-- Sidebar content here --> */}
+
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
