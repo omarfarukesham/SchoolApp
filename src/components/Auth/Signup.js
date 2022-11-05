d3de849762bb5f9ebb3b5259e00c8a60d442ab3e
Sakib Rahman <seu.sakib@gmail.com>
2022-11-05 12:46:46 +0600

    implemented react hook form

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Head from "../../asset/AuthImg/man-in-suit-and-tie.png";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
+import { useForm } from "react-hook-form";
 const Signup = () => {
+  const EMAIL_PATTERN = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-z]+)$/;
+  const PASSWORD_PATTERN = /^(?=.*\d).{8,}$/;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   const [signInWithGoogle, googleUser, googleLoading, googleError] =
     useSignInWithGoogle(auth);
+  const [createUserWithEmailAndPassword, emailUser, emailLoading, emailError] =
+    useCreateUserWithEmailAndPassword(auth);
   return (
     <div className="bg-[url('/src/asset/AuthImg/signupImg1.jpg')] h-screen bg-cover bg-no-repeat bg-center">
       <div className="flex justify-end h-screen ">
@@ -42,7 +57,7 @@ const Signup = () => {
                 </button>
               </div>
               {/* This will show google login error */}
-              <p className="mt-3 text-error text-center">{googleError.code}</p>
+              <p className="mt-3 text-error text-center">{googleError?.code}</p>
 
               <div className="flex mt-7 items-center text-center">
                 <hr className="border-gray-300 border-1 w-full rounded-md" />
@@ -54,7 +69,7 @@ const Signup = () => {
 
               {/* Form to Create user with email and password */}
               <div className="mt-8">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="flex flex-col mb-2">
                     <div className="flex relative ">
                       <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
@@ -65,10 +80,17 @@ const Signup = () => {
                         id="fullname"
                         className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                         placeholder="Your Full Name"
-                        required
+                        {...register("fullName", {
+                          required: "Enter your name",
+                        })}
                       />
                     </div>
                   </div>
+                  {errors.fullName && (
+                    <p className="text-red-500 text-sm mb-2 -mt-1">
+                      {errors.fullName?.message}
+                    </p>
+                  )}
                   <div className="flex flex-col mb-2">
                     <div className="flex relative ">
                       <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
@@ -84,14 +106,25 @@ const Signup = () => {
                       </span>
                       <input
                         type="text"
-                        id="email"
+                        // id="email"
                         className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        {...register("email", {
+                          required: "Enter your email",
+                          pattern: {
+                            value: EMAIL_PATTERN,
+                            message: "Enter valid email",
+                          },
+                        })}
                         placeholder="Your email"
-                        required
                       />
                     </div>
                   </div>
-                  <div className="flex flex-col mb-6">
+                  {errors.email && (
+                    <p className="text-red-500 text-sm mb-2 -mt-1">
+                      {errors.email?.message}
+                    </p>
+                  )}
+                  <div className="flex flex-col mb-2">
                     <div className="flex relative ">
                       <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                         <svg
@@ -106,13 +139,24 @@ const Signup = () => {
                       </span>
                       <input
                         type="password"
-                        id="pass"
+                        // id="pass"
                         className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                         placeholder="Your password"
-                        required
+                        {...register("password", {
+                          required: "Enter your password",
+                          pattern: {
+                            value: PASSWORD_PATTERN,
+                            message: "Min length 8 and at least 1 latter",
+                          },
+                        })}
                       />
                     </div>
                   </div>
+                  {errors.password && (
+                    <p className="text-red-500 text-sm pb-1">
+                      {errors.password?.message}
+                    </p>
+                  )}
 
                   <div className="flex w-full">
                     <button
